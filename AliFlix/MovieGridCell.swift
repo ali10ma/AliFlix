5aa7fce530e774c1e32ed11ce55775855143a7d0
Ali Ma <azym1025@gmail.com>
2021-02-14 16:00:37 -0800

    Added Superhero Network Request and added Collection View Setup

---
+++ AliFlix/MovieGridCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieGridCell.swift
+//  AliFlix
+//
+//  Created by Ali Ma on 2/14/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+   // @IBOutlet weak var posterView: UIImageView!
+    
+    //@IBOutlet weak var posterView: UIImageView!
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+    
+    
+}
--- AliFlix/MovieGridViewController.swift
+++ AliFlix/MovieGridViewController.swift
@@ -6,13 +6,76 @@
 //
 
 import UIKit
+import AlamofireImage
 
-class MovieGridViewController: UIViewController {
+class MovieGridViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    var movies = [[String:Any]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
 
         // Do any additional setup after loading the view.
+        //Replace url for superhero related movies
+        // let url = URL(string: //"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let url = URL(string: "https://api.themoviedb.org/3/movie/464052/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error)
+            in
+           // This will run when the network request returns
+           if let error = error {
+              print(error.localizedDescription)
+           } else if let data = data {
+              let dataDictionary = try!
+                JSONSerialization.jsonObject(with: data, options: [])as! [String: Any]
+            
+            //Download the above list of movies
+            //Store it to below line
+            //Casting as an array of dictionary, start from as![[]]
+            self.movies = dataDictionary["results"] as! [[String:Any]]
+            
+            self.collectionView.reloadData()
+            
+          //  self.tableView.reloadData()//its gonna call the function/data again!!
+            
+            print(dataDictionary)
+
+              // TODO: Get the array of movies
+              // TODO: Store the movies in a property to use elsewhere
+              // TODO: Reload your table view data
+
+           }
+        }
+        task.resume()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+       return movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieGridCell", for: indexPath) as! MovieGridCell
+        
+        let movie = movies[indexPath.item]
+        
+        //getting url from API
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl +
+                                posterPath)!
+        //Now Can give me the url, I'll download it & setup image
+        cell.posterView.af_setImage(withURL: posterUrl)
+        
+        
+        
+        return cell
     }
     
 
